54dfe52c1c77a76409e20a716986fbf77bfd3e91
Katarina Roberts <katarinarobertsux@gmail.com>
2022-05-04 11:52:52 +0100

    Update script.js
    update

--- script.js
+++ script.js
@@ -8,13 +8,8 @@ const amountEl = document.querySelector('#amount-el');
 const notesEl = document.querySelector('#notes-el');
 const totalEl = document.querySelector('#total-el');
 
-const deleteEl = document.querySelector('.deleteEl');
 
 let servicesToBuy = [];
-let taskElInnerHtml = taskEl.innerHTML;
-let amountElInnerHtml = amountEl.innerHTML;
-let notesElInnerHtml = notesEl.innerHTML;
-let totalElInnerHtml = totalEl.innerHTML;
 let total = 0;
 const service1 = ["Wash Car", 10];
 const service2 = ["Mow Lawn", 20];
@@ -26,50 +21,73 @@ function reset () {
     washBtn.disabled = false;
     mowBtn.disabled = false;
     weedsBtn.disabled = false;
-    // taskElInnerHTML = ``
+    taskEl.innerHTML='';
+    amountEl.innerHTML='';
+    notesEl.innerHTML='';
+    totalEl.innerHTML = `$${total}`
+};
+
+function mouseover (e) {
+    e.target.style.background = '#3A69D2';
+    e.target.style.color = 'white';
+    e.target.style.border = '#3A69D2';
+
 }
+function mouseleave (e) {
+    e.target.style.background = '';
+    e.target.style.color = '';
+    e.target.style.border = '';
+}
+
+function mouseup (e) {
+    e.target.style.background = '';
+    e.target.style.color = '';
+    e.target.style.border = '';
+}
+
+washBtn.addEventListener('mouseover', mouseover);
+washBtn.addEventListener('mouseleave', mouseleave)
+washBtn.addEventListener('mouseup', mouseup);
+
+mowBtn.addEventListener('mouseover', mouseover);
+mowBtn.addEventListener('mouseleave', mouseleave)
+mowBtn.addEventListener('mouseup', mouseup);
+
+weedsBtn.addEventListener('mouseover', mouseover);
+weedsBtn.addEventListener('mouseleave', mouseleave)
+weedsBtn.addEventListener('mouseup', mouseup);
 
 
 washBtn.addEventListener('click', function() {
-    console.log('Wash Car clicked');
     servicesToBuy.push(service1[0]);
-    taskEl.innerHTML += `<div class="deleteEl"><p>${service1[0]} added</p></div>`;
-    amountEl.innerHTML += `<div class="align-right deleteEl"><p>$${service1[1]}</p></div>`;
-    notesEl.innerHTML = `<div class="deleteEl"><p>We accept cash, credit card, or paypal</p></div>`
+    taskEl.innerHTML += `<div><p>${service1[0]} added</p></div>`;
+    amountEl.innerHTML += `<div class="align-right"><p>$${service1[1]}</p></div>`;
+    notesEl.innerHTML = `<div><p>We accept cash, credit card, or paypal</p></div>`
     total += service1[1];
     totalEl.innerHTML = `$${total}`;
-    console.log(`total is ${total}`);
-    console.log(servicesToBuy);
     washBtn.disabled = true;
 });
 
 mowBtn.addEventListener('click', function() {
-    console.log('Mow Lawn clicked');
     servicesToBuy.push(service2[0])
     taskEl.innerHTML += `<div><p>${service2[0]} added</p></div>`;
     amountEl.innerHTML += `<div class="align-right"><p>$${service2[1]}</p></div>`;
-    notesEl.innerHTML = `<div class="deleteEl"><p>We accept cash, credit card, or paypal</p></div>`
+    notesEl.innerHTML = `<div><p>We accept cash, credit card, or paypal</p></div>`
     total += service2[1];
     totalEl.innerHTML = `$${total}`;
-    console.log(`total is ${total}`);
-    console.log(servicesToBuy);
     mowBtn.disabled = true;
 });
 
 weedsBtn.addEventListener('click', function() {
-    console.log('Pull Weeds clicked');
     servicesToBuy.push(service3[0]);
     taskEl.innerHTML += `<div><p>${service3[0]} added</p></div>`;
     amountEl.innerHTML += `<div class="align-right"><p>$${service3[1]}</p></div>`;
-    notesEl.innerHTML = `<div class="deleteEl"><p>We accept cash, credit card, or paypal</p></div>`
+    notesEl.innerHTML = `<div><p>We accept cash, credit card, or paypal</p></div>`
     total += service3[1];
     totalEl.innerHTML = `$${total}`;
-    console.log(`total is ${total}`);
-    console.log(servicesToBuy);
     weedsBtn.disabled = true;
 });
 
 sendBtn.addEventListener('click', function(){
-    console.log('Send Invoice clicked');
     reset();
 })
