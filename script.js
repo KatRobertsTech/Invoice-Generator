77377e0acd42b469dd2154dafc8a771464ad7f9f
Katarina Roberts <katarinarobertsux@gmail.com>
2022-05-05 10:50:41 +0100

    Update script.js
    update

--- script.js
+++ script.js
@@ -8,12 +8,18 @@ const amountEl = document.querySelector('#amount-el');
 const notesEl = document.querySelector('#notes-el');
 const totalEl = document.querySelector('#total-el');
 
+// const removeEl = document.querySelectorAll('.remove');
+// removeEl.addEventListener('click', function(){
+//     console.log(removeEl);
+// });
+
 
 let servicesToBuy = [];
 let total = 0;
-const service1 = ["Wash Car", 10];
-const service2 = ["Mow Lawn", 20];
-const service3 = ["Pull Weeds", 30];
+// const service4 = ["Service Name", price, id]
+const service1 = ["Wash Car", 10, 1];
+const service2 = ["Mow Lawn", 20, 2];
+const service3 = ["Pull Weeds", 30, 3];
 
 function reset () {
     servicesToBuy = [];
@@ -45,6 +51,13 @@ function mouseup (e) {
     e.target.style.border = '';
 }
 
+// function removeWashBtn() {
+//     console.log('remove this');
+//     let taskToRemove = document.querySelectorAll(`.remove${service1[2]}`);
+//     console.log(taskToRemove);
+//     task.firstElementChild.innerHTML='';
+// }
+
 washBtn.addEventListener('mouseover', mouseover);
 washBtn.addEventListener('mouseleave', mouseleave)
 washBtn.addEventListener('mouseup', mouseup);
@@ -60,31 +73,35 @@ weedsBtn.addEventListener('mouseup', mouseup);
 
 washBtn.addEventListener('click', function() {
     servicesToBuy.push(service1[0]);
-    taskEl.innerHTML += `<div><p>${service1[0]} added</p></div>`;
-    amountEl.innerHTML += `<div class="align-right"><p>$${service1[1]}</p></div>`;
+    taskEl.innerHTML += `<div><p>${service1[0]}<span class="light-grey remove" onclick="removeWashBtn()">remove</span></p></div>`;
+    amountEl.innerHTML += `<div class="align-right"><p><span class="light-grey">$</span>${service1[1]}</p></div>`;
     notesEl.innerHTML = `<div><p>We accept cash, credit card, or paypal</p></div>`
     total += service1[1];
-    totalEl.innerHTML = `$${total}`;
+    totalEl.innerHTML = `<span class="light-grey">$</span>${total}`;
     washBtn.disabled = true;
 });
 
+
+// const removeEl = document.querySelectorAll('.remove');
+// console.log(removeEl);
+
 mowBtn.addEventListener('click', function() {
     servicesToBuy.push(service2[0])
-    taskEl.innerHTML += `<div><p>${service2[0]} added</p></div>`;
-    amountEl.innerHTML += `<div class="align-right"><p>$${service2[1]}</p></div>`;
+    taskEl.innerHTML += `<div><p>${service2[0]}<span class="light-grey remove">remove</span></p></div>`;
+    amountEl.innerHTML += `<div class="align-right"><p><span class="light-grey">$</span>${service2[1]}</p></div>`;
     notesEl.innerHTML = `<div><p>We accept cash, credit card, or paypal</p></div>`
     total += service2[1];
-    totalEl.innerHTML = `$${total}`;
+    totalEl.innerHTML = `<span class="light-grey">$</span>${total}`;
     mowBtn.disabled = true;
 });
 
 weedsBtn.addEventListener('click', function() {
     servicesToBuy.push(service3[0]);
-    taskEl.innerHTML += `<div><p>${service3[0]} added</p></div>`;
-    amountEl.innerHTML += `<div class="align-right"><p>$${service3[1]}</p></div>`;
+    taskEl.innerHTML += `<div><p>${service3[0]}<span class="light-grey remove">remove</span></p></div>`;
+    amountEl.innerHTML += `<div class="align-right"><p><span class="light-grey">$</span>${service3[1]}</p></div>`;
     notesEl.innerHTML = `<div><p>We accept cash, credit card, or paypal</p></div>`
     total += service3[1];
-    totalEl.innerHTML = `$${total}`;
+    totalEl.innerHTML = `<span class="light-grey">$</span>${total}`;
     weedsBtn.disabled = true;
 });
 
